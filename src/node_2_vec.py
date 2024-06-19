ce415353783c6a21b616e363f2ddb6fc7df94d3f
Ahmet Polat <polat2000@gmail.com>
2024-06-20 00:42:23 +0200

    init link_prediction

--- src/node_2_vec.py
+++ src/node_2_vec.py
@@ -1,78 +1,116 @@
+# external imports
+import networkx as nx
+import numpy as np
+import torch as th
+from torch.nn import Module, Parameter
+#import torch.nn.functional as F
+from torch.nn.init import kaiming_normal_, kaiming_uniform_, normal_, uniform_, xavier_normal_, xavier_uniform_
+from torch.optim import Adam
 from torch.utils.data import DataLoader
-import torch
-import torch.nn as nn
-import torch.optim as optim
+
+# internal imports
+from random_walks import RW_Iterable
 
 
-class Node2VecModel(nn.Module):
-    """
-    Implements a Node2Vec model using PyTorch's neural network module. This model learns low-dimensional 
-    embeddings for nodes in a graph based on their neighborhood. It is designed to be trained using the
-    Skip-Gram architecture, where the model predicts context nodes (positive samples) and non-context nodes
-    (negative samples) given a target node.
 
-    Attributes:
-        embeddings (nn.Embedding): An embedding layer that maps node indices to embedding vectors.
+class Node2Vec(Module):
+    '''node2vec embedding as torch module w/ embedding matrix X as parameter & the def. stochastic loss fct. as foward fct.'''
+    def __init__(self, n_nodes: int, dim: int, l: int) -> None:
+        super().__init__()
 
-    Args:
-        num_nodes (int): The total number of nodes in the graph. This determines the size of the embedding layer.
-        embedding_dim (int): The dimensionality of the embedding vectors.
+        # main attributerw_batch
+        #self.n_nodes = n_nodes  # number of nodes
+        #self.dim = dim  # embedding dimension
+        self.l = l  # random walk length
 
-    Methods:
-        forward(start_node, walk, negative_samples):
-            Performs a forward pass of the Node2Vec model using the input data.
+        # initialize node2vec embedding matrix X randomly as parameter
+        self.X = Parameter(th.empty(n_nodes, dim))  # n_nodes x dim
+        # TODO test different initializations?
+        kaiming_normal_(self.X)
+        #kaiming_uniform_(self.X)
+        #normal_(self.X, mean=0.0, std=1.0)
+        #uniform_(self.X, a=-1.0, b=1.0)
+        #xavier_normal_(self.X)
+        #xavier_uniform_(self.X)
 
-        loss(positive_probs, negative_probs):
-            Computes the loss of the model using the binary cross-entropy loss function.
-    """
 
-    embeddings: nn.Embedding
+    def forward(self, rw_batch: th.Tensor) -> th.Tensor:
+        '''forward fct. of node2vec embedding, takes batch matrix (2D) of stacked pq-walk data, returns scalar value of mean loss fct. over pq-walk batch (simplified, see conversion) as def. in sheet/script'''
+        sum_loss = th.tensor(0.)  # initialize sum of loss values
+        for rw_vec in list(rw_batch):  # run over pq-walk data vectors in batch
+            X_start = self.X[rw_vec[0]]  # embedding vec. of start node (X_s)
+            walk_idx = rw_vec[: self.l + 1]  # selection from X acc. to pq-walk nodes, including start node
+            neg_idx = rw_vec[self.l + 1 :]  # selection from X acc. to negative samples
+            numerator_term = th.sum(th.matmul(self.X[walk_idx[1 :]], X_start), -1)  # see conversion of loss-fct., using walk_idx w/o start node
 
-    def __init__(self, num_nodes: int, embedding_dim: int) -> None:
-        """
-        Initializes the Node2VecModel instance by setting up the embedding layer.
-        """
-        super(Node2VecModel, self).__init__()
-        self.embeddings = nn.Embedding(num_nodes, embedding_dim)
+            # FIXME whether to reassign walk_idx to only include each node once (i.e. interpret pq-walk "w" as set rather than sequence in denominator term), interpretation in script/sheet unclear, enable/disable accordingly
+            walk_idx = th.tensor(list(set(np.array(walk_idx))))
 
-    from typing import Tuple
+            # add loss value for each pq-walk: compute denominator term (see conversion of loss-fct.), then subtract numerator term
+            sum_loss += self.l * th.log(th.sum(th.exp(th.matmul(self.X[th.cat([walk_idx, neg_idx], -1)], X_start)), -1)) - numerator_term
 
-    def forward(self, start_node: torch.Tensor, walk: torch.Tensor, negative_samples: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Forward pass of the Node2Vec model. Computes embeddings for start nodes, walk nodes, and negative samples.
+        return sum_loss / len(rw_batch)  # return mean loss over batch
 
-        Args:
-            start_node (torch.Tensor): A tensor containing indices of start nodes.
-            walk (torch.Tensor): A tensor containing indices of walk nodes that are typically neighbors of the start nodes.
-            negative_samples (torch.Tensor): A tensor containing indices of negative samples which are not neighbors.
+"""
+    def forward(self, rw_batch: th.Tensor) -> th.Tensor:
+        '''short version of forward fct. of node2vec embedding, interpreting pq-walk "w" as sequence rather than set within denominator term'''
+        sum_loss = th.tensor(0.)  # initialize sum of loss values
+        for rw_vec in list(rw_batch):  # run over pq-walk data vectors in batch
+            X_start = self.X[rw_vec[0]]  # embedding vec. of start node (X_s)
+            # add loss value for each pq-walk all in one go
+            sum_loss += self.l * th.log(th.sum(th.exp(th.matmul(self.X[rw_vec], X_start)), -1)) - th.sum(th.matmul(self.X[rw_vec[1 : self.l + 1]], X_start), -1)
+        return sum_loss / len(rw_batch)  # return mean loss over batch
+"""
 
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor]: A tuple containing two tensors with probabilities of positive and negative samples.
-        """
-        embedded_start = self.embeddings(start_node).unsqueeze(1)
-        embedded_walk = self.embeddings(walk)
-        embedded_neg_samples = self.embeddings(negative_samples)
 
-        positive_scores = torch.sum(embedded_start * embedded_walk, dim=-1)
-        negative_scores = torch.sum(
-            embedded_start * embedded_neg_samples, dim=-1)
+def train_node2vec(dataset: RW_Iterable, dim: int, l: int,  # main parameters, see sheet
+                   n_batches: int, batch_size: int, device: str,  # extra parameters
+                   lr: float = 0.001) -> np.ndarray:  # default learning rate
+    '''trains node2vec model on given graph w/ Adam optimizer, using [batch_size] random walks w/ parameters p, q, l, l_ns & embedding [dim]'''
+    # prepare dataloader, model & optimizer
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=0)  # single-process
+    model = Node2Vec(dataset.n_nodes, dim, l)  # construct model object
+    model.to(device)  # move model to device
+    model.train()  # switch model to training mode
+    optimizer = Adam(model.parameters(), lr=lr)  # construct optimizer
 
-        positive_probs = torch.sigmoid(positive_scores)
-        negative_probs = torch.sigmoid(-negative_scores)
+    # stream [n_batches] random pq-walk batches W from custom iterable dataset & train model on them successively
+    for i in range(n_batches):
+        for rw_batch in dataloader:  # access single batch in dataloader
+            rw_batch = rw_batch.to(device)  # move random walk batch to device
+            #loss = th.mean(model(W), 0)  # forward pass = compute loss, take batch-lvl mean (see sheet/script)
+            loss = model(rw_batch)  # # forward pass = compute batch-lvl mean loss
+            loss.backward()  # backward pass
+            optimizer.step()  # SGD step
+            optimizer.zero_grad()  # set gradients to zero
 
-        return positive_probs, negative_probs
+    model.eval()  # switch model to evaluation mode (optional?)
+    return model.get_parameter('X').detach().numpy()  # return embedding matrix X as np.ndarray
+    #return model.get_parameter('X').detach().to('cpu').numpy()  # move back to CPU first?
 
-    def loss(self, positive_probs: torch.Tensor, negative_probs: torch.Tensor) -> torch.Tensor:
-        """
-        Computes the model's loss using binary cross-entropy.
 
-        Args:
-            positive_probs (torch.Tensor): Probabilities associated with positive samples.
-            negative_probs (torch.Tensor): Probabilities associated with negative samples.
 
-        Returns:
-            torch.Tensor: The computed loss value, as a single tensor.
-        """
-        positive_loss = -torch.log(positive_probs + 1e-10).mean()
-        negative_loss = -torch.log(1 - negative_probs + 1e-10).mean()
-        return positive_loss + negative_loss
+if __name__ == "__main__":
+    # test node2vec embedding
+    import pickle
+
+    device = ("cuda" if th.cuda.is_available() else "mps" if th.backends.mps.is_available() else "cpu")  # choose by device priority
+    batch_size = 10
+    n_batches = 100
+    dim = 12
+    p = 1.0
+    q = 1.0
+    l = 5
+    l_ns = 5
+    set_node_labels = False
+
+    with open('datasets/Citeseer/data.pkl', 'rb') as data:
+    #with open('datasets/Cora/data.pkl', 'rb') as data:
+    #with open('datasets/Facebook/data.pkl', 'rb') as data:  # cannot construct self.node_labels for Facebook, idk why, not needed tho
+    #with open('datasets/PPI/data.pkl', 'rb') as data:
+        graph = pickle.load(data)[0]
+
+    dataset = RW_Iterable(graph, p, q, l, l_ns, batch_size, set_node_labels)  # custom iterable dataset instance
+    #dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=0)  # turns random walk batches into th.tensors, single-process w/ renewing randomness
+    X = train_node2vec(dataset, dim, l, n_batches, batch_size, device)
+    print(X[0])
